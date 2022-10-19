bc597346f2a331ae683f702abc13805a8759cfe3
adam00jeff <107988571+adam00jeff@users.noreply.github.com>
2022-10-19 10:23:07 +0100

    Add files via upload

---
+++ Painter/Painter/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Painter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(31, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "The Button";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 484);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "This Button";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Painter/Painter/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Painter
+{/// <summary>
+/// partial class split over multiple source files
+/// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+        Bitmap myBitmap;
+        Pen myPen = new Pen(Color.Black, 3);
+        public Form1()
+        {
+            InitializeComponent();
+            ///should be same size as the form
+            myBitmap = new Bitmap(640, 480);
+
+        }
+        /// <summary>
+        /// 
+        /// button 1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("button pressed");
+            button1.Text = "Stop";
+            myPen = new Pen(Color.Red, 2);
+        }
+        /// <summary>
+        /// 
+        /// button 2 
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("button2 pressed");
+            button2.Text = "Stop stop";
+            myPen = new Pen(Color.Blue, 2);
+        }
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; // get graphics context of form
+            g.DrawImageUnscaled(myBitmap, 0, 0); // put off-screen bitmap on form
+            Refresh();// refresh the window to display
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(myBitmap); // get graphics context of screen bitmap
+            ///Pen p = myPen(Color.Red, 2);
+            g.DrawLine(myPen, e.X, e.Y, e.X + 1, e.Y + 1); // draw point on off screen bitmap
+        }
+    }
+}
